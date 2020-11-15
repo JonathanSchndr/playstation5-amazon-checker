158f3b29e462dd29d95aea0fd49cd79c1341e679
Jonathan Schneider <jonathan@jott.media>
2020-11-15 13:33:55 +0100

    add files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # playstation5-amazon-checker
-this node tool check, if the playstation 5 on amazon available 
+this programme checks in the background if the playstation 5 is available on amazon. if it is available, the programme speaks to you ;)
+
+## install
+- download the repo
+- cd into the dir
+- npm install
+- node index.js
+
+## info
+the program is adapted for the german market, if you want it to check availability for your country, you have to adapt the url and the string for the check
+other country, needs a change:
+- DETECTION_STRING_NOT_AVAILABLE
+- PLAYSTATION_DIGITAL_URL
+- PLAYSTATION_DRIVE_URL
---
+++ index.js
@@ -0,0 +1,31 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const say = require("say")
+
+const DETECTION_STRING_NOT_AVAILABLE = "Derzeit nicht verfügbar."; //OTHER COUNTRY, NEEDS A CHANGE
+const PLAYSTATION_DIGITAL_URL = "https://www.amazon.de/Sony-PlayStation-5-Digital-Edition/dp/B08H98GVK8/"; //OTHER COUNTRY, NEEDS A CHANGE
+const PLAYSTATION_DRIVE_URL = "https://www.amazon.de/Sony-Interactive-Entertainment-PlayStation-5/dp/B08H93ZRK9/"; //OTHER COUNTRY, NEEDS A CHANGE
+const INTERVAL = 10000;
+const AVAILABLE_MESSAGE = " ist auf Amazon verfügbar!";
+
+setInterval(() => {
+
+  request({
+    uri: PLAYSTATION_DIGITAL_URL
+  }, function (error, response, body) {
+    var $ = cheerio.load(body);
+    var availbleDigital = $("#availability > span").text().indexOf(DETECTION_STRING_NOT_AVAILABLE);
+    var productNameDigital = $("#productTitle").text();
+    if (availbleDigital < 0) say.speak(productNameDigital + AVAILABLE_MESSAGE);
+  });
+
+  request({
+    uri: PLAYSTATION_DRIVE_URL
+  }, function (error, response, body) {
+    var $ = cheerio.load(body);
+    var availbleDrive = $("#availability > span").text().indexOf(DETECTION_STRING_NOT_AVAILABLE);
+    var productNameDrive = $("#productTitle").text();
+    if (availbleDrive < 0) say.speak(productNameDrive + AVAILABLE_MESSAGE);
+  });
+
+}, INTERVAL);
