e22a8a5992d020d8c2090bc1b6f3d46d77a57ae7
Jonathan Schneider <jonathan@jott.media>
2020-11-16 11:18:20 +0100

    changer interval, bot protection

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const say = require("say")
 const DETECTION_STRING_NOT_AVAILABLE = "Derzeit nicht verfügbar."; //OTHER COUNTRY, NEEDS A CHANGE
 const PLAYSTATION_DIGITAL_URL = "https://www.amazon.de/Sony-PlayStation-5-Digital-Edition/dp/B08H98GVK8/"; //OTHER COUNTRY, NEEDS A CHANGE
 const PLAYSTATION_DRIVE_URL = "https://www.amazon.de/Sony-Interactive-Entertainment-PlayStation-5/dp/B08H93ZRK9/"; //OTHER COUNTRY, NEEDS A CHANGE
-const INTERVAL = 10000;
+const INTERVAL = 60000;
 const AVAILABLE_MESSAGE = " ist auf Amazon verfügbar!";
 
 setInterval(() => {
