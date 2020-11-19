fd98c0d94396830c908ed71c7b6deac2bc6ded26
Jonathan Schneider <jonathan@jott.media>
2020-11-19 10:44:51 +0100

    add open browser if avaible

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const request = require("request");
 const cheerio = require("cheerio");
 const say = require("say")
+const open = require("open")
 
 const DETECTION_STRING_NOT_AVAILABLE = "Derzeit nicht verfügbar."; //OTHER COUNTRY, NEEDS A CHANGE
 const PLAYSTATION_DIGITAL_URL = "https://www.amazon.de/dp/B08H98GVK8/"; //OTHER COUNTRY, NEEDS A CHANGE
 const PLAYSTATION_DRIVE_URL = "https://www.amazon.de/dp/B08H93ZRK9/"; //OTHER COUNTRY, NEEDS A CHANGE
-const INTERVAL = 60000;
+const INTERVAL = 10000;
 const AVAILABLE_MESSAGE = " ist auf Amazon verfügbar!";
+
+let isBrowserOpen = false;
 
 setInterval(() => {
 
@@ -16,7 +19,13 @@ setInterval(() => {
     var $ = cheerio.load(body);
     var availbleDigital = $("#availability > span").text().indexOf(DETECTION_STRING_NOT_AVAILABLE);
     var productNameDigital = $("#productTitle").text();
-    if (availbleDigital < 0 && productNameDigital != "") say.speak(productNameDigital + AVAILABLE_MESSAGE);
+    if (availbleDigital > 0 && productNameDigital != "") {
+      say.speak(productNameDigital + AVAILABLE_MESSAGE);
+      if (!isBrowserOpen) open(PLAYSTATION_DIGITAL_URL, {
+        app: 'google chrome'
+      });
+      isBrowserOpen = true;
+    }
   });
 
   request({
@@ -25,7 +34,13 @@ setInterval(() => {
     var $ = cheerio.load(body);
     var availbleDrive = $("#availability > span").text().indexOf(DETECTION_STRING_NOT_AVAILABLE);
     var productNameDrive = $("#productTitle").text();
-    if (availbleDrive < 0 && productNameDrive != "") say.speak(productNameDrive + AVAILABLE_MESSAGE);
+    if (availbleDrive < 0 && productNameDrive != "") {
+      say.speak(productNameDrive + AVAILABLE_MESSAGE);
+      if (!isBrowserOpen) open(PLAYSTATION_DRIVE_URL, {
+        app: 'google chrome'
+      });
+      isBrowserOpen = true;
+    }
   });
 
 }, INTERVAL);
