b3080965a84c819dcec716c20dc4b7e17bcfe044
Benjamin Geissler <geissbnj@schaeffler.com>
2020-11-26 14:34:02 +0100

    - refactor to use axios cause request is deprecated - use promises - a loop used for scraping several uris - scraping the price for information purposes - use few es6 features

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
-const request = require("request");
+const axios = require("axios").default;
 const cheerio = require("cheerio");
-const say = require("say")
-const open = require("open")
+const say = require("say");
+const fs = require("fs");
+const colors = require("colors");
+const open = require("open");
 
 const DETECTION_STRING_NOT_AVAILABLE = "Derzeit nicht verfügbar."; //OTHER COUNTRY, NEEDS A CHANGE
+const AVAILABLE_MESSAGE = "Auf Lager.";
+
 const PLAYSTATION_DIGITAL_URL = "https://www.amazon.de/dp/B08H98GVK8/"; //OTHER COUNTRY, NEEDS A CHANGE
 const PLAYSTATION_DRIVE_URL = "https://www.amazon.de/dp/B08H93ZRK9/"; //OTHER COUNTRY, NEEDS A CHANGE
+const PLAYSTATION_4_URL =
+  "https://www.amazon.de/PlayStation-Konsole-F-Chassis-DualShock-Controller/dp/B07HHPX4N1?ref_=ast_sto_dp"; //OTHER COUNTRY, NEEDS A CHANGE
+const PRODUCTS_TO_CHECK = [
+  PLAYSTATION_DRIVE_URL,
+  PLAYSTATION_DIGITAL_URL,
+  PLAYSTATION_4_URL,
+];
+const BROWSER = "google chrome";
 const INTERVAL = 30000;
-const AVAILABLE_MESSAGE = " ist auf Amazon verfügbar!";
 
 let isBrowserOpen = false;
 
 setInterval(() => {
-
-  request({
-    uri: PLAYSTATION_DIGITAL_URL
-  }, function (error, response, body) {
-    var $ = cheerio.load(body);
-    var availbleDigital = $("#availability > span").text().indexOf(DETECTION_STRING_NOT_AVAILABLE);
-    var productNameDigital = $("#productTitle").text();
-    if (availbleDigital < 0 && productNameDigital != "") {
-      say.speak(productNameDigital + AVAILABLE_MESSAGE);
-      if (!isBrowserOpen) open(PLAYSTATION_DIGITAL_URL, {
-        app: 'google chrome'
-      });
-      isBrowserOpen = true;
-    }
-  });
-
-  request({
-    uri: PLAYSTATION_DRIVE_URL
-  }, function (error, response, body) {
-    var $ = cheerio.load(body);
-    var availbleDrive = $("#availability > span").text().indexOf(DETECTION_STRING_NOT_AVAILABLE);
-    var productNameDrive = $("#productTitle").text();
-    if (availbleDrive < 0 && productNameDrive != "") {
-      say.speak(productNameDrive + AVAILABLE_MESSAGE);
-      if (!isBrowserOpen) open(PLAYSTATION_DRIVE_URL, {
-        app: 'google chrome'
-      });
-      isBrowserOpen = true;
-    }
-  });
-
+  PRODUCTS_TO_CHECK.forEach(scrapProduct);
 }, INTERVAL);
+
+async function scrapProduct(uri, i) {
+  const AVAILABLE_SELECTOR = "#availability > span";
+
+  try {
+    const product_page = (
+      await axios.get(uri, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36",
+        },
+      })
+    ).data;
+
+    const $ = cheerio.load(product_page);
+    const title = $("#productTitle").text().trim();
+    const price = $("#priceblock_ourprice").text().trim();
+    const availableText = $(AVAILABLE_SELECTOR).text().trim();
+
+    console.log(`Checking for ${title} `);
+
+    if (availableText === AVAILABLE_MESSAGE) {
+      console.log(colors.rainbow(`Price at: ${price} `));
+      say.speak(title + AVAILABLE_MESSAGE);
+      open(uri, { app: BROWSER });
+    } else if (availableText === DETECTION_STRING_NOT_AVAILABLE) {
+      console.log(colors.red(availableText));
+    }
+  } catch (error) {
+    console.error(`Error while scraping ${uri}`, error);
+  } finally {
+    console.log(colors.zebra(`------------`));
+  }
+}
